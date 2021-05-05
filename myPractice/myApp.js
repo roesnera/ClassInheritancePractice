502b26a8bb0af53661d1782fe52bb854651300ea
Ashley U <ashley.umetsu@gmail.com>
2021-05-05 09:36:06 -0500

    Adding Dev and Cook classes

--- myPractice/myApp.js
+++ myPractice/myApp.js
@@ -1,10 +1,32 @@
 class Person() {
-    constructor() {
+    constructor(name, age, numBooksRead) {
         this.name = name;
         this.age = age;
         this.numBooksRead = numBooksRead;
     }
     readNewBook() {
         this.numBooksRead++;
+    }
+}
+
+class Developer extends Person {
+    constructor(name, age, numBooksRead) {
+        super(name, age, numBooksRead);
+        this.numKnownProgrammingLang = numKnownProgrammingLang;
+    }
+}
+
+class Cook extends Person {
+    constructor(name, age, numBooksRead) {
+        super(name, age, numBooksRead);
+        this.isHungry = isHungry;
+    } 
+
+    eat () {
+        if(this.isHungry) {
+            this.isHungry = false;
+        } else {
+            console.log("Ugh, I'm so full.");
+        }
     }
 }
